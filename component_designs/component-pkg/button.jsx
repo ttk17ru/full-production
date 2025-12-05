ee6f7e2e665c080b975f859245629f879a9ba973
TTK <electronano285@gmail.com>
2025-12-05 17:42:22 +0200

    adding button component commit

---
+++ component_designs/component-pkg/button.jsx
@@ -0,0 +1,15 @@
+function normalButton(text, onClickFunc) {
+  return (
+    <>
+      <button onClick={onClickFunc} class="nor-button">{text}</button> 
+    </>
+  )
+}
+
+function normalButton(text, onClickFunc) {
+  return (
+    <>
+      <button onClick={onClickFunc} class="pro-button">{text}</button> 
+    </>
+  )
+}
